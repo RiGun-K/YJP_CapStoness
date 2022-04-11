a0d251331d70cd1041ac05c647169d2ed6b52176
RiGun-K <1234suho@naver.com>
2022-04-11 11:31:17 +0900

    2022/04/11 1차 통합

---
+++ src/main/java/com/example/yjp_capstone/YjpCapStoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.yjp_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjpCapStoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjpCapStoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yjp_capstone/YjpCapStoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yjp_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjpCapStoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
